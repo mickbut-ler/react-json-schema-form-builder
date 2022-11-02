91abfdf0281f4ef76021e6604cf3845753f1cec4
mick <mickbutlerr@gmail.com>
2022-11-02 09:27:26 +0100

    replaced package

--- example/src/JsonSchemaFormSuite.js
+++ example/src/JsonSchemaFormSuite.js
@@ -13,7 +13,7 @@ import { Theme as Bootstrap4Theme } from '@rjsf/bootstrap-4';
 import {
   FormBuilder,
   PredefinedGallery,
-} from '@ginkgo-bioworks/react-json-schema-form-builder';
+} from 'rid-json';
 import withStyles from 'react-jss';
 import Tabs from './tabs/Tabs';
 import JSONInput from 'react-json-editor-ajrm';
